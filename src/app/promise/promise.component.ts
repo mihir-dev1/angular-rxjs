2e617654f5d6c950cc08ada428ca661a4ec6e863
mihir-dev1 <mihirtrivedi002@gmail.com>
2021-11-14 13:16:57 +0530

    add promise component

--- src/app/promise/promise.component.ts
+++ src/app/promise/promise.component.ts
@@ -7,9 +7,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PromiseComponent implements OnInit {
 
+  public promiseValue:any;
+
   constructor() { }
 
+  dellLaptop() {
+    return false;
+  }
+
+  hpLaptop() {
+    return true;
+  }
+
   ngOnInit(): void {
+    // available
+    let buyLaptop = new Promise((resolve, rejects) => {
+      // resolve('Promise is resolved');
+      // rejects('Promise is rejects');
+      if (this.dellLaptop()) {
+        setTimeout(() => {
+          resolve('Dell Laptop Purchased');
+        }, 3000);
+      } else if (this.hpLaptop()) {
+        setTimeout(() => {
+          resolve('Hp Laptop Purchased');
+        }, 3000);
+      } else {
+        setTimeout(() => {
+          rejects('Laptop not Available');
+        }, 2000);
+      }
+    })
+
+    buyLaptop.then(res => {
+      this.promiseValue = res;
+      console.log('then code =>', res);
+    }).catch(res => {
+      this.promiseValue = res;
+      console.log('catch code =>', res);
+    })
   }
 
 }
